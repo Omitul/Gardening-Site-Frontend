32133ebe280bc59640b5baaa2fe86556d0609c72
Omitul <omitul1010@gmail.com>
2024-10-10 19:51:04 +0600

    change password page done

---
+++ src/app/change-password/layout.tsx
@@ -0,0 +1,7 @@
+export default function changePasswordLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ src/app/change-password/page.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Slide, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { getUser, updateUser } from "@/src/services/authService";
+import bcrypt from "bcryptjs";
+
+export default function NewPassword() {
+  const router = useRouter();
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const user = await getUser();
+      if (!user) {
+        toast.error("User not found.");
+        setLoading(false);
+        return;
+      }
+
+      const isMatch = await bcrypt.compare(currentPassword, user?.password);
+      if (!isMatch) {
+        toast.error("Current password is incorrect.");
+        setLoading(false);
+        return;
+      }
+
+      if (newPassword !== confirmPassword) {
+        toast.error("New password and confirmation password do not match.");
+        setLoading(false);
+        return;
+      }
+
+      const hashedPassword: string = await bcrypt.hash(newPassword, 10);
+      const response = await updateUser(user._id, {
+        password: hashedPassword,
+      });
+
+      if (response) {
+        toast.success("Password updated successfully.");
+
+        router.push("/dashboard/profile");
+      } else {
+        toast.error("Failed to update password.");
+      }
+    } catch (error) {
+      console.error("Error updating password:", error);
+      toast.error("An error occurred while updating the password.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full">
+        <h3 className="text-center text-2xl font-semibold mb-6">
+          Update Password
+        </h3>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="old-password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Current Password
+            </label>
+            <input
+              id="new-password"
+              type="password"
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              className="mt-1 w-full p-3 border border-gray-300 rounded-lg focus:ring focus:ring-green-500 focus:border-green-500"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="new-password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              New Password
+            </label>
+            <input
+              id="new-password"
+              type="password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              className="mt-1 w-full p-3 border border-gray-300 rounded-lg focus:ring focus:ring-green-500 focus:border-green-500"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="confirm-password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm New Password
+            </label>
+            <input
+              id="confirm-password"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="mt-1 w-full p-3 border border-gray-300 rounded-lg focus:ring focus:ring-green-500 focus:border-green-500"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className={`w-full bg-green-500 text-white py-3 rounded-lg hover:bg-green-600 transition ${
+              loading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={loading}
+          >
+            {loading ? "Updating..." : "Change Password"}
+          </button>
+        </form>
+      </div>
+      <ToastContainer transition={Slide} />
+    </div>
+  );
+}
